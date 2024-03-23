f0cc9ba7835249b019c80399adf0c4d44a69954b
Benjamin Barbier <benjamin.barbier.59@gmail.com>
2024-03-23 14:30:58 +0100

    ✨ generate countries resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CharactersModule } from './characters/characters.module';
+import { CountriesModule } from './countries/countries.module';
 
 @Module({
-  imports: [CharactersModule],
+  imports: [CharactersModule, CountriesModule],
   controllers: [AppController],
   providers: [AppService],
 })
