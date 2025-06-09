e11204006410e07a11fbdbc52ab3e4552492464e
Benjamin Barbier <benjamin@tech-homies.io>
2025-06-09 23:02:43 +0200

    📝 add examples to fight DTO properties

--- src/fights/dto/fight.dto.ts
+++ src/fights/dto/fight.dto.ts
@@ -3,7 +3,10 @@ import { Transform } from 'class-transformer';
 import { IsDate, IsNotEmpty, IsString } from 'class-validator';
 
 export class FightDto {
-  @ApiProperty({ description: 'Identifiant du premier combattant' })
+  @ApiProperty({
+    description: 'Identifiant du premier combattant',
+    example: 'ryu',
+  })
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) =>
@@ -11,7 +14,10 @@ export class FightDto {
   )
   characterOneId: string;
 
-  @ApiProperty({ description: 'Identifiant du second combattant' })
+  @ApiProperty({
+    description: 'Identifiant du second combattant',
+    example: 'ken',
+  })
   @IsString()
   @IsNotEmpty()
   @Transform(({ value }) =>
@@ -19,12 +25,18 @@ export class FightDto {
   )
   characterTwoId: string;
 
-  @ApiProperty({ description: 'Identifiant du gagnant du combat' })
+  @ApiProperty({
+    description: 'Identifiant du gagnant du combat',
+    example: 'ryu',
+  })
   @IsString()
   @IsNotEmpty()
   winnerId: string;
 
-  @ApiProperty({ description: 'Date du combat' })
+  @ApiProperty({
+    description: 'Date du combat',
+    example: '2023-10-01T12:00:00Z',
+  })
   @IsDate()
   @IsNotEmpty()
   date: Date;
